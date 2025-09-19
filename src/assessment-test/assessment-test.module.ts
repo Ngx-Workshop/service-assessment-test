4cbd0888c0eb1c2ffe89b57b8b3e11606d1a2a1a
Wesley DuSell <dusellfam@gmail.com>
2025-09-18 23:49:04 -0400

    Fake Providers for Swagger

--- src/assessment-test/assessment-test.module.ts
+++ src/assessment-test/assessment-test.module.ts
@@ -1,6 +1,6 @@
 import { Module } from '@nestjs/common';
-import { MongooseModule } from '@nestjs/mongoose';
-import { NgxAuthClientModule } from '@tmdjr/ngx-auth-client';
+import { MongooseModule, getModelToken } from '@nestjs/mongoose';
+import { NgxAuthClientModule, RemoteAuthGuard } from '@tmdjr/ngx-auth-client';
 import { AssessmentTestController } from './assessment-test.controller';
 import { AssessmentTestService } from './assessment-test.service';
 import {
@@ -13,15 +13,55 @@ import {
 } from './schemas/user-assessment-test.schemas';
 import { UserAssessmentTestService } from './user-assessment-test.service';
 
+const ASSESSMENT_SCHEMA_IMPORTS =
+  process.env.GENERATE_OPENAPI === 'true'
+    ? []
+    : [
+        MongooseModule.forFeature([
+          { name: AssessmentTest.name, schema: AssessmentTestSchema },
+          { name: UserAssessmentTest.name, schema: UserAssessmentTestSchema },
+        ]),
+      ];
+
+// When generating OpenAPI, stub out Mongoose models (and guard, if referenced)
+const ASSESSMENT_FAKE_PROVIDERS =
+  process.env.GENERATE_OPENAPI === 'true'
+    ? [
+        {
+          provide: getModelToken(AssessmentTest.name),
+          useValue: {
+            find: () => ({ exec: async () => [] }),
+            findById: () => ({ exec: async () => null }),
+            findByIdAndUpdate: () => ({ exec: async () => null }),
+            findOne: () => ({ exec: async () => null }),
+            create: async () => ({}),
+          },
+        },
+        {
+          provide: getModelToken(UserAssessmentTest.name),
+          useValue: {
+            find: () => ({ exec: async () => [] }),
+            findById: () => ({ exec: async () => null }),
+            findByIdAndUpdate: () => ({ exec: async () => null }),
+            findOne: () => ({ exec: async () => null }),
+            create: async () => ({}),
+          },
+        },
+        {
+          // In case guards are evaluated during bootstrap, make it a no-op
+          provide: RemoteAuthGuard,
+          useValue: { canActivate: () => true },
+        },
+      ]
+    : [];
+
 @Module({
-  imports: [
-    NgxAuthClientModule,
-    MongooseModule.forFeature([
-      { name: AssessmentTest.name, schema: AssessmentTestSchema },
-      { name: UserAssessmentTest.name, schema: UserAssessmentTestSchema },
-    ]),
-  ],
+  imports: [NgxAuthClientModule, ...ASSESSMENT_SCHEMA_IMPORTS],
   controllers: [AssessmentTestController],
-  providers: [AssessmentTestService, UserAssessmentTestService],
+  providers: [
+    AssessmentTestService,
+    UserAssessmentTestService,
+    ...ASSESSMENT_FAKE_PROVIDERS,
+  ],
 })
 export class AssessmentTestModule {}
